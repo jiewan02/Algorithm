dd1f7380f710cd89c11b494e4a0b929af7781a0e
jiewan02 <85870517+jiewan02@users.noreply.github.com>
2025-09-09 22:31:35 +0900

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,6 +1,6 @@
 def solution(my_string, m, c):
-    result = ''
-    for i in range(0, len(my_string), m): 
-        result += my_string[i + c - 1]
+    #result = ''
+    #for i in range(0, len(my_string), m): 
+    #    result += my_string[i + c - 1]
         
-    return result
+    return my_string[c-1::m]

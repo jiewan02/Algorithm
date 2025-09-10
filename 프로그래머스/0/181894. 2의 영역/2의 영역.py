3311a712b118c68c8b327cb46d8a26be4126e84f
jiewan02 <85870517+jiewan02@users.noreply.github.com>
2025-09-10 16:43:30 +0900

    [level 0] Title: 2의 영역, Time: 9.87 ms, Memory: 14.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -0,0 +1,16 @@
+def solution(arr):
+    result = []
+    new_result = []
+    idx = -1
+    for i in range(len(arr)): 
+        if arr[i] == 2:
+            result.append(i)  
+            
+    if not result:
+        return [-1]
+    if len(result) == 1: 
+        return [arr[result[0]]]
+    else: 
+        result.sort()
+        return arr[result[0]: result[-1]+1]
+    

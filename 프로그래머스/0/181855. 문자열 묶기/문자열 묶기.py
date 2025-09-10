96b4a40d0ae52b8816cda740ba6ef276fbdd510a
jiewan02 <85870517+jiewan02@users.noreply.github.com>
2025-09-10 15:29:04 +0900

    [level 0] Title: 문자열 묶기, Time: 17.31 ms, Memory: 15.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -0,0 +1,11 @@
+def solution(strArr):
+    count_dict = {}
+    for s in strArr:
+        length = len(s)
+        if length in count_dict:
+            count_dict[length] += 1 
+        else:
+            count_dict[length] = 1  
+    
+    #return count_dict
+    return max(count_dict.values())

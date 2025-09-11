fe94eb72ea040b7e0247380399c0f280cebcfc66
jiewan02 <85870517+jiewan02@users.noreply.github.com>
2025-09-11 11:17:38 +0900

    [level 0] Title: 배열 만들기 6, Time: 357.55 ms, Memory: 17.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,16 @@
+def solution(arr):
+    stk = []
+    i = 0
+    while i < len(arr): 
+        if len(stk) == 0: 
+            stk.append(arr[i])
+        elif len(stk) != 0 and stk[-1] == arr[i]: 
+            stk.pop()
+        elif len(stk) != 0 and stk[-1] != arr[i]: 
+            stk.append(arr[i])
+        i += 1
+        
+    if stk:   
+        return stk
+    else:
+        return [-1]

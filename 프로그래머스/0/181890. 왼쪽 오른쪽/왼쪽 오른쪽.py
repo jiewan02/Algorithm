126c7ba1602e3b27b3f8860d37c69e4cee37cf3d
jiewan02 <85870517+jiewan02@users.noreply.github.com>
2025-09-11 13:43:10 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -2,6 +2,6 @@ def solution(str_list):
     for i, s in enumerate(str_list): 
         if s == 'l': 
             return str_list[:i]
-        if s == 'r':
+        elif s == 'r':
             return str_list[i+1:]
     return []

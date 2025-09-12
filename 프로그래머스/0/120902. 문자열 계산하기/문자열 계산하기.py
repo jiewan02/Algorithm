ad0d4219435b55b6ca402011033668749df41a1f
jiewan02 <85870517+jiewan02@users.noreply.github.com>
2025-09-12 11:08:45 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.03 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -4,13 +4,12 @@ def solution(my_string):
     answer = int(my_string[0])
     
     for i in range(1, len(my_string), 2): 
-        if not my_string[i].isdigit():
-            if my_string[i] == '+': 
-                answer += int(my_string[i+1])
-                i += 1
-            else: 
-                answer -= int(my_string[i+1])
-                i += 1
-                
+        if my_string[i] == '+': 
+            answer += int(my_string[i+1])
+            #i += 1
+        else: 
+            answer -= int(my_string[i+1])
+            #i += 1
+        i += 1   
     return answer
             

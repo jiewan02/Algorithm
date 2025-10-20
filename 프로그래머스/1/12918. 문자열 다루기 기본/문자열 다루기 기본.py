6a02c69fb198f9f2c451519dbc9718cce0ea195a
jiewan02 <85870517+jiewan02@users.noreply.github.com>
2025-10-20 09:33:32 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,10 +1,10 @@
 def solution(s):
-    answer = True
+    
     if len(s) != 4 and len(s) != 6: 
         return False
     
     for i in s: 
         if i.isalpha(): 
             return False
-            break
-    return answer
+        
+    return True

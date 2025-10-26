09a7446c701e3f8f4bc9b84b44b4604084b73588
jiewan02 <85870517+jiewan02@users.noreply.github.com>
2025-10-26 22:15:34 +0900

    [level 1] Title: 실패율, Time: 1634.11 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,21 +1,13 @@
-def solution(N, stages):
-    total_users = len(stages)
-    result = []
-    # 각 스테이지에 멈춘 인원 수 계산
-    people = [0] * (N + 2)
-    for stage in stages:
-        people[stage] += 1
-
-    challenger = total_users
-    for i in range(1, N + 1):
-        if challenger == 0:
-            fail_rate = 0
-        else:
-            fail_rate = people[i] / challenger
-        result.append((i, fail_rate))
-        challenger -= people[i]   # 다음 스테이지로 넘어갈 사람 수
-
-    # 실패율 높은 순, 실패율 같으면 스테이지 번호 작은 것 먼저
-    result.sort(key=lambda x: (-x[1], x[0]))
-
-    return [r[0] for r in result]
+def solution(N, stages): 
+    result = {}
+    denom = len(stages)
+    
+    for stage in range(1, N+1): 
+        if denom != 0: 
+            count = stages.count(stage)
+            result[stage] = count / denom 
+            denom -= count
+        else: 
+            result[stage] = 0
+            
+    return sorted(result, key = lambda x: result[x], reverse=True)

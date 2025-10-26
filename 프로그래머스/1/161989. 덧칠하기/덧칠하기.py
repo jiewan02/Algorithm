bb57e305a6ed79e998c66e4f1ff1927509a7e2af
jiewan02 <85870517+jiewan02@users.noreply.github.com>
2025-10-26 21:15:55 +0900

    [level 1] Title: 덧칠하기, Time: 3.35 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,10 +1,10 @@
 def solution(n, m, section):
-    answer = 0
-    idx = 0
-    length = len(section)
-    while idx < length:
-        coverage = section[idx] + m - 1
-        answer += 1
-        while idx < length and section[idx] <= coverage:
-            idx += 1
+    answer = 1 
+    prev = section[0]
+    
+    for i in section: 
+        if i - prev >= m: 
+            prev = i
+            answer += 1
+            
     return answer

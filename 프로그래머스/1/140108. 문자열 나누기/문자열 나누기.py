f91c9dccde1085e868ffda9e1a0109598217f5ab
Jiewan <85870517+jiewan02@users.noreply.github.com>
2025-11-01 19:01:25 +0900

    [level 1] Title: 문자열 나누기, Time: 0.87 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,23 +1,13 @@
-def solution(s):
+def solution(s): 
     answer = 0
-    i = 0
-
-    while i < len(s):
-        first = s[i]
-        same = 0
-        diff = 0
-
-        for j in range(i, len(s)):
-            if s[j] == first:
-                same += 1
-            else:
-                diff += 1
-            if same == diff:
-                answer += 1
-                i = j + 1
-                break
-        else:
+    same = 0
+    diff = 0
+    for i in s: 
+        if same == diff: 
             answer += 1
-            break
-
+            a = i
+        if a == i: 
+            same += 1
+        else: 
+            diff += 1
     return answer

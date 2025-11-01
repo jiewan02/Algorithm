60ca40ebe536491f2208814d2b1dac2f0b02a48b
Jiewan <85870517+jiewan02@users.noreply.github.com>
2025-11-01 18:09:31 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.04 ms, Memory: 9.48 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,29 +1,15 @@
 def solution(dartResult):
-    score = []
-    i = 0
-    length = len(dartResult)
-    while i < length:
-        if dartResult[i].isdigit():
-            if dartResult[i] == '1' and i+1 < length and dartResult[i+1] == '0':
-                num = 10
-                i += 1  
-            else:
-                num = int(dartResult[i])
-            i += 1
-            if dartResult[i] == 'S':
-                num = num ** 1
-            elif dartResult[i] == 'D':
-                num = num ** 2
-            elif dartResult[i] == 'T':
-                num = num ** 3
-            if i+1 < length and dartResult[i+1] in ['*','#']:
-                if dartResult[i+1] == '*':
-                    num *= 2
-                    if score:
-                        score[-1] *= 2  
-                elif dartResult[i+1] == '#':
-                    num *= -1
-                i += 1 
-            score.append(num)
-        i += 1
-    return sum(score)
+    dart = {'S': 1, 'D':2, 'T': 3}
+    scores = []
+    n = 0
+    
+    for i, d in enumerate(dartResult): 
+        if d in dart: 
+            scores.append(int(dartResult[n:i]) ** dart[d])
+        if d == '*': 
+            scores[-2:] = [x*2 for x in scores[-2:]]
+        if d == '#': 
+            scores[-1] *= -1
+        if not (d.isnumeric()): 
+            n = i + 1
+    return sum(scores)

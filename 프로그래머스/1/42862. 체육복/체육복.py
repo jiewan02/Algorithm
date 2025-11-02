a19230e0f81df1f29d509a1d7d8f1c6051fb5e62
Jiewan <85870517+jiewan02@users.noreply.github.com>
2025-11-02 19:32:41 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,12 +1,12 @@
 def solution(n, lost, reserve):
-    # 겹치는 학생을 set으로 뽑아내고, 각각에서 제거
     overlap = set(lost) & set(reserve)
-    lost = [l for l in lost if l not in overlap]
-    reserve = [r for r in reserve if r not in overlap]
-
-    for r in sorted(reserve):  # 작은 번호부터 빌려주기!
-        if r-1 in lost:
-            lost.remove(r-1)
-        elif r+1 in lost:
-            lost.remove(r+1)
+    lost = [i for i in lost if i not in overlap]
+    reserve = [i for i in reserve if i not in overlap]
+    
+    for i in sorted(reserve): 
+        if i-1 in lost: 
+            lost.remove(i-1)
+        elif i+1 in lost: 
+            lost.remove(i+1)
+        
     return n - len(lost)

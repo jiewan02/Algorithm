a9a1cb7537712f0e6f0eebeecc4704502c9decdd
Jiewan <85870517+jiewan02@users.noreply.github.com>
2025-11-02 19:18:36 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.07 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,7 +1,5 @@
-def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    for p, c in zip(participant, completion):
-        if p != c:
-            return p
-    return participant[-1]  # 마지막 사람이 완주 못한 경우
+import collections
+
+def solution(participant, completion): 
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]

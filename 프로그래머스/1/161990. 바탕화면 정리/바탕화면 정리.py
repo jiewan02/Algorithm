08e087d25b69aec15115b1f37e0bef0ed5831109
Jiewan <85870517+jiewan02@users.noreply.github.com>
2025-11-02 20:32:00 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.69 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,18 +1,8 @@
-def solution(wallpaper):
-    rows = len(wallpaper)
-    cols = len(wallpaper[0])
-
-    # 파일 좌표 모두 모으기
-    points = [(i, j)
-              for i in range(rows)
-              for j in range(cols)
-              if wallpaper[i][j] == "#"]
-
-    # 각 축에서 min, max
-    min_row = min(p[0] for p in points)
-    min_col = min(p[1] for p in points)
-    max_row = max(p[0] for p in points)
-    max_col = max(p[1] for p in points)
-
-    # 끝점은 max+1 (격자점이므로)
-    return [min_row, min_col, max_row+1, max_col+1]
+def solution(wallpaper): 
+    a, b = [], []
+    for i in range(len(wallpaper)): 
+        for j in range(len(wallpaper[i])): 
+            if wallpaper[i][j] == '#': 
+                a.append(i)
+                b.append(j)
+    return [min(a), min(b), max(a) + 1, max(b) + 1]

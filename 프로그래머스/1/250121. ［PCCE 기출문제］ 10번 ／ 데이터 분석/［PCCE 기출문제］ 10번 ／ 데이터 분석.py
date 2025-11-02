c1b7fdc4f8cc22ee1b2147025a10d9c69e6dd8f3
Jiewan <85870517+jiewan02@users.noreply.github.com>
2025-11-02 19:51:10 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.11 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,12 +1,8 @@
 def solution(data, ext, val_ext, sort_by):
-    # 컬럼명과 인덱스 매칭 (문제 설명대로 순서 지정)
-    col_index = {'code': 0, 'date': 1, 'maximum': 2, 'remain': 3}
-    ext_idx = col_index[ext]
-    sort_idx = col_index[sort_by]
-
-    # 1. 조건에 맞는 것만 필터링
-    filtered = [row for row in data if row[ext_idx] < val_ext]
-
-    # 2. 정렬
-    sorted_data = sorted(filtered, key=lambda x: x[sort_idx])
+    col_idx = {'code': 0, 'date': 1, 'maximum': 2, 'remain': 3}
+    ext_idx = col_idx[ext]
+    sort_idx = col_idx[sort_by]
+    
+    filtered = [i for i in data if i[ext_idx] < val_ext]
+    sorted_data = sorted(filtered, key = lambda x: x[sort_idx])
     return sorted_data

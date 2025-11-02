6e1a8b1be79249b63e3494fa46c8b4ec77e0965b
Jiewan <85870517+jiewan02@users.noreply.github.com>
2025-11-02 20:02:12 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.40 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,19 +1,18 @@
 def solution(board, moves):
-    basket = []
+    stacklist = []
     answer = 0
-    n = len(board)
-    
-    for move in moves:
-        col = move - 1  # 0-based index
-        for row in range(n):  # 위->아래로 순회
-            if board[row][col] != 0:
-                doll = board[row][col]
-                board[row][col] = 0  # 인형 뽑아서 빈 칸으로
-                # 바구니 터지기 처리
-                if basket and basket[-1] == doll:
-                    basket.pop()
-                    answer += 2  # 2개 터짐
-                else:
-                    basket.append(doll)
+
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i-1] != 0:
+                stacklist.append(board[j][i-1])
+                board[j][i-1] = 0
+
+                if len(stacklist) > 1:
+                    if stacklist[-1] == stacklist[-2]:
+                        stacklist.pop(-1)
+                        stacklist.pop(-1)
+                        answer += 2     
                 break
+
     return answer

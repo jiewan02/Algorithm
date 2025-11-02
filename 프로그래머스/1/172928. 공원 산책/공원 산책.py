bd3458c2eef0cf91a16cb4e5c441b14c8c10d457
Jiewan <85870517+jiewan02@users.noreply.github.com>
2025-11-02 21:19:07 +0900

    [level 1] Title: 공원 산책, Time: 0.26 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -1,32 +1,12 @@
 def solution(park, routes):
+    y, x = [(i, j) for i, row in enumerate(park) for j, v in enumerate(row) if v == 'S'][0]
+    d = {'N': (-1, 0), 'S': (1, 0), 'W': (0, -1), 'E': (0, 1)}
     H, W = len(park), len(park[0])
-    # 시작점 찾기
-    for i in range(H):
-        for j in range(W):
-            if park[i][j] == 'S':
-                y, x = i, j
-
-    # 방향 dictionary
-    d = {'N':(-1,0), 'S':(1,0), 'W':(0,-1), 'E':(0,1)}
-
     for route in routes:
-        op, n = route.split()
-        dy, dx = d[op]
+        dir, n = route.split()
+        dy, dx = d[dir]
         n = int(n)
-        ny, nx = y + dy * n, x + dx * n
-
-        # 먼저 목적지 벗어나는지 체크
-        if not (0 <= ny < H and 0 <= nx < W):
-            continue
-
-        # 이동할 경로에 장애물이 있는지 체크
-        ok = True
-        for k in range(1, n+1):
-            cy, cx = y + dy * k, x + dx * k
-            if park[cy][cx] == 'X':
-                ok = False
-                break
-        if ok:
-            y, x = ny, nx
-
+        tmp = [(y + dy*k, x + dx*k) for k in range(1, n+1)]
+        if all(0 <= ny < H and 0 <= nx < W and park[ny][nx] != 'X' for ny, nx in tmp):
+            y, x = tmp[-1]
     return [y, x]

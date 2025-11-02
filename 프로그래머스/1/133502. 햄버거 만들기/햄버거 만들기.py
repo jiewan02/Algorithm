b57aae05eb2b965e4da40fd6a428af90ab98dba4
Jiewan <85870517+jiewan02@users.noreply.github.com>
2025-11-02 19:36:11 +0900

    [level 1] Title: 햄버거 만들기, Time: 217.27 ms, Memory: 25.7 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,11 +1,11 @@
 def solution(ingredient):
-    answer = 0
-    stack = []
-    for ing in ingredient:
-        stack.append(ing)
-        # 햄버거 패턴 체크: stack의 마지막 4개가 [1,2,3,1]이면
-        if stack[-4:] == [1,2,3,1]:
-            answer += 1
-            # 4개 빼주기
-            stack[-4:] = []
-    return answer
+    s = []
+    count = 0
+    for i in ingredient: 
+        s.append(i) 
+        if s[-4:] == [1, 2, 3, 1]: 
+            count += 1
+            for i in range(4): 
+                s.pop()
+                
+    return count

7739ce15f7f6dd798037b728bcd926f7c06a1525
Jiewan <85870517+jiewan02@users.noreply.github.com>
2025-12-05 01:02:34 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,14 @@
+def solution(brown, yellow):
+    total = brown + yellow
+    
+    for height in range(3, total + 1): 
+        if total % height != 0: 
+            continue
+            
+        width = total // height
+        
+        if width < height: 
+            continue
+        
+        if (width-2) * (height-2) == yellow: 
+            return [width, height]

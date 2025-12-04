097307e058fb4df9d8dbfb30db6197b0ddb8e91d
Jiewan <85870517+jiewan02@users.noreply.github.com>
2025-12-04 20:23:00 +0900

    [level 1] Title: 모의고사, Time: 3.16 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,23 +1,22 @@
 def solution(answers):
-    # 1, 2, 3번 찍는 패턴
     p1 = [1, 2, 3, 4, 5]
     p2 = [2, 1, 2, 3, 2, 4, 2, 5]
     p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
     scores = [0, 0, 0]
-    result = []
     
     for idx, answer in enumerate(answers): 
-        if answer == p1[idx % len(p1)]:
+        if answer == p1[idx % len(p1)]: 
             scores[0] += 1
-        if answer == p2[idx % len(p2)]:
+        if answer == p2[idx % len(p2)]: 
             scores[1] += 1
         if answer == p3[idx % len(p3)]: 
             scores[2] += 1
-    
-    for idx, s in enumerate(scores): 
-        if s == max(scores):
-            result.append(idx + 1)
             
-    return result
-
+    max_score = max(scores)
+    result = []
+    
+    for i, s in enumerate(scores): 
+        if s == max_score: 
+            result.append(i+1)
     return result
